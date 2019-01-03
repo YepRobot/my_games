9e3e0d41da08668c1355f230964720de485eb8ad
Stray-Alex <1005218593@qq.com>
2019-01-03 11:39:12 +0800

    客户端向服务端传送数据，json落子

--- NetConfig.py
+++ NetConfig.py
@@ -105,9 +105,12 @@ class NetServer(QObject):
         threading.Thread(target=self.__acceptConnect).start()
 
     def __acceptConnect(self):
+        try:
+            self.cli_socket, cli_addr = self.socket.accept()
+        except:
+            pass
         while True:
             try:
-                self.cli_socket, cli_addr = self.socket.accept()
 
                 data = self.cli_socket.recv(4096).decode()
                 self.msg_signal.emit(data)
--- NetPlayerGame.py
+++ NetPlayerGame.py
@@ -2,6 +2,9 @@
 # @author: alex  
 # @time: 2019/1/3 9:35
 from my_games.DoublePlayerGame import DoublePlayerGame
+import json
+from PyQt5.QtCore import *
+from my_games.DoublePlayerGame import *
 
 
 class NetPlayerGame(DoublePlayerGame):
@@ -16,5 +19,57 @@ class NetPlayerGame(DoublePlayerGame):
         self.close()
         self.net_object.socket.close()
 
+    '''
+    {
+        "msg_type":"position",
+        "x":"10",
+        "y":"15",
+        "color":"black"
+    }
+    
+    '''
+
     def parseData(self, data):
-        print(data)
+        print('parseData' + data)
+        msg = json.loads(data)
+        print('msg:', msg)
+        if msg['msg_type'] == "position":
+            self.downChessman(QPoint(int(msg['x']), int(msg['y'])), msg['color'])
+
+    def downChessman(self, point, color):
+
+        chess_index = (point.y(), point.x())  # 棋子在棋盘中的下标
+        pos = QPoint(50 + point.x() * 30, 50 + point.y() * 30)  # 棋子在棋盘中的坐标
+        pos_x = pos.x()
+        pos_y = pos.y()
+
+        self.chessman = Chessman(color=color, parent=self)
+        # self.chessman.setIndex(chess_index[1], chess_index[0])
+        self.chessman.move(pos_x, pos_y)
+        self.chessman.show()  # 显示棋子
+
+        # 显示标识
+        self.focus_Point.move(pos_x, pos_y)
+        self.focus_Point.show()
+        self.focus_Point.raise_()
+
+        self.chessman.map_point_x = chess_index[0]
+        self.chessman.map_point_y = chess_index[1]
+        print("zheshi" + str(self.chessman.map_point_x) + str(self.chessman.map_point_y))
+
+        if self.chessman.color == 0:
+            self.chessman.color = 'black'
+        else:
+            self.chessman.color = 'white'
+
+        self.chess_map[self.chessman.map_point_x][self.chessman.map_point_y] = self.chessman
+
+        # 历史记录
+        self.history_chess.append(self.chessman)
+
+        # 改变落子颜色
+        if self.color_flag == 0:
+            self.color_flag = 1
+        else:
+            self.color_flag = 0
+        # 判断输赢
